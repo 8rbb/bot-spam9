2b7c61543a6fb7246b1cf33f908b8281b1ab96ca
8rbb <42538720+8rbb@users.noreply.github.com>
2018-11-28 02:41:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+var prefix = "9";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "510819302388137988"; // ايدي السررفر
+var channel = "517092772923179008";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**8rb | Spammmmmmmmmmmmm **')
     },305);
 })
 
